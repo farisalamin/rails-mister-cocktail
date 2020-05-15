41899f9995f4a3b43cddd12fe8e6f8b8e7412eac
Faris Alamin <fma3210@gmail.com>
2020-05-15 15:46:05 +0100

    added image upload functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients_json = open(url).read
+ingredients = JSON.parse(ingredients_json)["drinks"]
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
